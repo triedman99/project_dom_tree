26ca13675275a90c937e55496b1961d623949075
Tamal Riedman <triedman99@gmail.com>
2016-09-17 02:43:08 -0700

    Modified TagParser to use hash and struct

--- parse_tag.rb
+++ parse_tag.rb
@@ -1,29 +1,30 @@
+Tags = Struct.new(:type, :classes, :id, :name)
 
 class ParseTag
 
   def initialize(string)
     @string = string
+    @tag_hash = {}
   end
 
   def type
-    puts @string.match(/^<([a-z]*\d?)/).captures.join
+    @tag_hash[:type] = @string.match(/^<([a-z]*\d?)/).captures.join
   end
 
   def classes
-    puts @string.match(/class[ = ]*'(.*?)'/).captures.join
+    @tag_hash[:classes] = @string.match(/class[ = ]*'(.*?)'/).captures.join.split(' ')
   end
 
   def id
-    puts @string.match(/id[ = ]*'(.*?)'/).captures.join
+    @tag_hash[:id] = @string.match(/id[ = ]*'(.*?)'/).captures.join
   end
 
   def name
-    puts @string.match(/name[ = ]*'(.*?)'/).captures.join
+    @tag_hash[:name] = @string.match(/name[ = ]*'(.*?)'/).captures.join
   end
+
 end
 
-tag = ParseTag.new("<p class= 'foo bar' id = 'baz' name ='fozzie'>Hello</p>")
-tag.type
-tag.classes
-tag.id
-tag.name
+tag = ParseTag.new("<p class= 'foo bar' id = 'head' name ='Tamal'>Hello</p>")
+html = Tags.new(tag.type, tag.classes, tag.id, tag.name)
+p html
