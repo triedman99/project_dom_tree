f4cf2a0c09f1c2d22f43e7a958fb95294b78433d
Tamal Riedman <triedman99@gmail.com>
2016-09-18 16:51:14 -0700

    Simplify Tag Parser and Warmup II(doesn't output indentation)

---
+++ html_parser.rb
@@ -0,0 +1,57 @@
+Node = Struct.new(:tag, :children, :parent)
+
+class HTMLParser
+
+  def initialize(html)
+    @root = Node.new({:type => 'document'}, [], nil)
+    @html = html
+    parse
+  end
+
+  def tags
+    @html.scan(/<.*?>/)
+  end
+
+  def text
+    @html.split(/<.*?>/)
+  end
+
+  def parse
+    tag_array = tags
+    text_array = text[1..-1]
+    current_parent = @root
+    current_node = Node.new(tag_array.shift, [], @root)
+    current_parent.children << current_node
+    current_node.children << text_array.shift
+    until tag_array.empty?
+      if tag_array[0].match(/\/#{current_node.tag[1..-2]}/)
+        current_node = current_node.parent
+        tag_array.shift
+        current_node.children << text_array.shift
+      else
+        current_parent = current_node
+        current_node = Node.new(tag_array.shift, [], current_parent)
+        current_parent.children << current_node
+        current_node.children << text_array.shift
+      end
+    end
+    @root = @root.children[0]
+    outputter(@root)
+  end
+
+  def outputter(node)
+    current_root = node
+    puts current_root.tag
+    current_root.children.each do |child|
+      if child.is_a?(String)
+        puts child
+      else
+        outputter(child)
+      end
+    end
+    puts "</#{current_root.tag[1..-2]}>"
+  end
+
+end
+
+parser = HTMLParser.new("<div> div text before <p> p text </p> <div> more div text </div> div text after </div>")
--- parse_tag.rb
+++ parse_tag.rb
@@ -1,30 +1,10 @@
-Tags = Struct.new(:type, :classes, :id, :name)
-
-class ParseTag
-
-  def initialize(string)
-    @string = string
-    @tag_hash = {}
-  end
-
-  def type
-    @tag_hash[:type] = @string.match(/^<([a-z]*\d?)/).captures.join
-  end
-
-  def classes
-    @tag_hash[:classes] = @string.match(/class[ = ]*'(.*?)'/).captures.join.split(' ')
-  end
-
-  def id
-    @tag_hash[:id] = @string.match(/id[ = ]*'(.*?)'/).captures.join
-  end
-
-  def name
-    @tag_hash[:name] = @string.match(/name[ = ]*'(.*?)'/).captures.join
-  end
-
+def parse_tag(string)
+  tag_hash = {}
+  tag_hash[:type] = string.match(/^<([a-z]*\d?)/).captures.join
+  tag_hash[:classes] = string.match(/class[ = ]*'(.*?)'/).captures.join.split(' ')
+  tag_hash[:id] = string.match(/id[ = ]*'(.*?)'/).captures.join
+  tag_hash[:name] = string.match(/name[ = ]*'(.*?)'/).captures.join
+  p  tag_hash
 end
 
-tag = ParseTag.new("<p class= 'foo bar' id = 'head' name ='Tamal'>Hello</p>")
-html = Tags.new(tag.type, tag.classes, tag.id, tag.name)
-p html
+parse_tag("<p class = 'foo bar' id = 'baz' name = 'fozzie'>")
